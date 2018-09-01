2d081ccb24f60bf03900676ec116b39beac6a2e8
SANDIP CHITALE <sandipchitale@gmail.com>
2018-09-01 18:59:47 +0100

    Use programatic enablement of page action.

--- jwt.js
+++ jwt.js
@@ -30,6 +30,25 @@
     },
     ["requestHeaders"]);
 
+    chrome.runtime.onInstalled.addListener(function () {
+        // Replace all rules ...
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+            // With a new rule ...
+            chrome.declarativeContent.onPageChanged.addRules([
+                {
+                    // That fires when a page's URL contains a 'g' ...
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: { schemes: ['http', 'https'] },
+                        })
+                    ],
+                    // And shows the extension's page action.
+                    actions: [new chrome.declarativeContent.ShowPageAction()]
+                }
+            ]);
+        });
+    });
+
     chrome.runtime.onMessage.addListener(function(message, sender, responseCallback) {
         if (message.requestsForTabId) {
             responseCallback(tabIdFrameIdToTokens[message.requestsForTabId]);
